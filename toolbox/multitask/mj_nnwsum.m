8786d6f67515d99a632f7adc9b3a54f885059737
Francisco Manuel Castro Payán <fmcp19@gmail.com>
2018-02-01 13:10:31 +0100

    Update mj_nnwsum.m

--- toolbox/multitask/mj_nnwsum.m
+++ toolbox/multitask/mj_nnwsum.m
@@ -36,40 +36,6 @@ else
    end
 end
 
-
-% if isempty(dzdy) %forward
-%    a1 = 0.5 * sum(res(:,posIdx) .^2,1);
-%    Ypos = a1* (1.0/nsamples);
-%    
-%    Yneg = 0.5 * (max(0, m - sqrt(sum(res(:,negIdx).^2,1)))).^2;
-%    Y = sum(Ypos) + sum(Yneg);
-% else
-%    
-%    % Init
-%    Y_ = zerosLike(X2);
-%    Y2_ = zerosLike(X2);
-%    
-%    % For positive pairs
-%    Y_(1,1,:,posIdx)= -1.* res(:,posIdx);
-%    Y = single (Y_ * (dzdy / n) );
-%    
-%    Y2_(1,1,:,posIdx)= res(:,posIdx);
-%    Y2 = single (Y2_ * (dzdy / n) );
-%    
-%    % For negative pairs
-%    resN = res(:,negIdx);
-%    Yneg = zerosLike(resN);
-%    N = sqrt(sum(resN.^2,1));      % Euclidean distance
-%    
-%    below = find(N <= m);
-%    bN = repmat(N(below),[size(resN,1),1]);
-%    Yneg(:,below) = + (m - bN).*(resN(:,below)./bN); % First minus is correct
-%    Y(1,1,:,negIdx) = single (Yneg * (dzdy / n) );
-%    
-%    Y2neg = -Yneg;
-%    Y2(1,1,:,negIdx) = single (Y2neg * (dzdy / n) );
-% end
-
 % --------------------------------------------------------------------
 function y = zerosLike(x)
 % --------------------------------------------------------------------
